76fb82385e63d58607bebd84915be3cec704b8c4
Yimi Zhao <yimi@seeice.com>
2015-01-29 10:44:21 -0500

    update VacationCost.py

--- VacationCost.py
+++ VacationCost.py
@@ -30,15 +30,17 @@ def car_rental_cost(days):
 		return 40*days
 
 #total cost
-def trip_cost(city,days):
-	 return car_rental_cost(days)+hotel_cost(days)+flight_cost(city)
+def trip_cost(city,days,spending_money):
+	 return car_rental_cost(days)+hotel_cost(days)+flight_cost(city)+spending_money
 
 
 # calculate your vacation cost
 trip_city = raw_input("choose your trip city(Charlotte, Tampa, Pittsburgh or Los Angeles) :")
 trip_days = raw_input("how many days you are going to stay?")
 trip_length = int(trip_days)
-totalCost = trip_cost(trip_city, trip_length)
-print "The total cost of your lovely trip will be %s, Bon Voyage! " %totalCost
+money = raw_input("how much money you plan to spent during this trip :)")
+money = int(money)
+totalCost = trip_cost(trip_city, trip_length,money)
+print "The total cost of your lovely trip will be %s, Bon Voyage & Happy Shopping! " %totalCost
 
 
