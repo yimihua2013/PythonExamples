7eec39a5869ad3bec67fe7e0c72079c1cf638330
Yimi Zhao <yimi@seeice.com>
2015-01-31 01:01:13 -0500

    add guess_a_numer.py

---
+++ guess_a_number.py
@@ -0,0 +1,30 @@
+# Guess_a_number Game
+
+from random import randint
+
+# welcome info & game rule
+print("Welcome to the game!")
+rules = """ 
+Game rules:
+(1)First, the program will randomly choose a numer from 1 to 10;
+(2)Next, you bets a guess, if the guess is right, you win, otherwise, you lose. 
+(3)In total, you have 3 chances to win.
+"""
+print(rules)
+print("Good luck!")
+
+# choose a random integer in 1:100
+theNumber = randint(1,10) 
+
+# 5 times for player to win
+times = 3
+
+while times > 0:
+	 guess = int(raw_input("Your guess:"))
+	 if guess == theNumber:
+	 	print ("You are awsome,you win!")
+	 	break
+	 times -= 1
+else:
+	print ("You lose! The right number is : " + str(theNumber))
+     
